5cb8291fa746227cbf8d43d6cd6d841c4792794d
ForestParadiseOrigen <andres.jcv.678@gmail.com>
2024-05-02 12:46:31 -0500

    ADD - Nuevo log agregado y ajuste de espacios en proyectos personales

--- src/templates/components/models/proyectos/proyectosModel.jsx
+++ src/templates/components/models/proyectos/proyectosModel.jsx
@@ -2,13 +2,13 @@ import './proyectosStyles.css'
 
 const ProyectosModel = ({data}) => {
     return(
-        <section key={data.id} className="w-128">
+        <section key={data.id} className="min-w-92 w-92 pb-5">
             <header className="flex flex-row items-end w-full py-1 justify-end cursor-default">
                 <span className="after:duration-500 duration-500 border-2 border-rose-600 px-1 hover:bg-rose-600 text-white">{data.state}</span>
             </header>
 
             <section className=" border-y-2 border-rose-600 p-1 flex flex-row flex-nowrap gap-1">
-                <section className="">
+                <section>
                     <article className="flex flex-row flex-nowrap gap-2">
                         <svg className=" bg-rose-600 w-fit" xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"><path fill="white" d="m13.061 4.939-2.122 2.122L15.879 12l-4.94 4.939 2.122 2.122L20.121 12z"></path><path fill="white" d="M6.061 19.061 13.121 12l-7.06-7.061-2.122 2.122L8.879 12l-4.94 4.939z"></path></svg>
                         <h1 className="text-white text-2xl font-semibold p-2 bg-rose-600 text-nowrap w-full">{data.nombre}</h1>
@@ -17,11 +17,11 @@ const ProyectosModel = ({data}) => {
                         <p className="text-md py-2 h-24 overflow-y-auto text-white">{data.descripcion}</p>
                     </article>
                 </section>
-                <aside className="hover:bg-rose-600 hover:text-white text-md bg-white p-1 text-vertical whitespace-nowrap font-semibold" href="https://boxicons.com/?query=che"><a href={data.link}>GITHUB</a></aside>
+                <aside className="hover:bg-rose-600 hover:text-white text-md bg-white p-1 text-vertical whitespace-nowrap font-semibold" href="https://boxicons.com/?query=che"><a href={data.link} target="_blank">GITHUB</a></aside>
             </section>
 
             <footer className="flex flex-row justify-end">
-                <img src="/src/assets/img/QR/SVG/danger-line.svg"/>
+                <img className='h-full bg-repeat-x' src="/src/assets/img/QR/SVG/danger-line.svg"/>
             </footer>
         </section>
     );
